1e84182a15a45cfbbdf8a61fb957820ace8b867d
Tony Baines <tony.baines.1@gmail.com>
2011-03-20 22:24:38 +0000

    Added JQuery

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -35,6 +35,9 @@ grails.views.default.codec = "none" // none, html, base64
 grails.views.gsp.encoding = "UTF-8"
 // enable Sitemesh preprocessing of GSP pages
 grails.views.gsp.sitemesh.preprocess = true
+
+grails.views.javascript.library="jquery"
+
 // scaffolding templates configuration
 grails.scaffolding.templates.domainSuffix = 'Instance'
 
